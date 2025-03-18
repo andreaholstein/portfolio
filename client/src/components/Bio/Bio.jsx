b93f0c0e74450dea7966e3863a80ca4354ba33ac
andrea <andrea.gitcommit@gmail.com>
2025-03-18 16:58:16 -0400

    Build Static Bio Component, Build Template for Bubble Components

---
+++ client/src/components/Bio/Bio.jsx
@@ -0,0 +1,18 @@
+// ---------- FXNALITY ----------
+// import { useState, useEffect } from 'react'
+// ---------- COMPONENTS ----------
+
+// ---------- STYLES ----------
+import "./Bio.scss";
+
+export default function Bio() {
+
+    return (
+        <section className="bio">
+            <h2 className="bio__title title">Bio</h2>
+            <div className="bio__bubble">
+                <p className="bio__blurb">Andrea Holstein is a Toronto-based full stack software engineer, with a background in React web application builds. A lover of frontend builds, Andrea is elite at flexboxing and padding a pixel-perfect mockup to life.</p>
+            </div>
+        </section>
+    );
+}
