6d8f1161b433b756fa9e4f2ba62f16be20704b47
andrea <andrea.gitcommit@gmail.com>
2025-07-31 20:40:23 -0400

    Updated hamburger nav menu styling and functionality

--- client/src/components/NavLinks/NavLinks.jsx
+++ client/src/components/NavLinks/NavLinks.jsx
@@ -1,18 +1,25 @@
 // ---------- FXNALITY ----------
 // import { useState, useEffect } from 'react'
+import { Link } from "react-router";
 // ---------- COMPONENTS ----------
-
 // ---------- STYLES ----------
 import "./NavLinks.scss";
 
 export default function NavLinks() {
-
+    // TO DO: Update Links with actual pathways, ensure nav menu panel closes on click!\
+    // MVP: No hamburger menu, all one page app?
     return (
         <section className="nav">
             <ul className="nav__list">
-                <li className="nav__links">Home</li>
-                <li className="nav__links">Bio</li>
-                <li className="nav__links">Contact</li>
+                <Link to="/">
+                    <li className="nav__links">Home</li>
+                </Link>
+                <Link to="/">
+                    <li className="nav__links">Bio</li>
+                </Link>
+                <Link to="/">
+                    <li className="nav__links">Contact</li>
+                </Link>
             </ul>
         </section>
     );
