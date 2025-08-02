fb9a3452f59d2fa3f5691870f6c556da70b71ede
andrea <andrea.gitcommit@gmail.com>
2025-08-02 15:14:51 -0400

    Building accordion logic - trying for active state and show content fxns

--- client/src/components/Project/Project.jsx
+++ client/src/components/Project/Project.jsx
@@ -1,16 +1,18 @@
 // ---------- FXNALITY ----------
-import { useState, useEffect } from 'react'
+// import { useState, useEffect } from 'react'
 // ---------- COMPONENTS ----------
 // ---------- STYLES ----------
 import "./Project.scss";
 
+
 export default function Project({ project }) {
+
     return (
         <div className="project">
-            <h3 className="project__title">{project.title}</h3>
-            <h3 className="project__role">{project.role}</h3>
-            <h3 className="project__company">{project.productionCompany}</h3>
-            <h3 className="project__venue">{project.venue}</h3>
+            <h4 className="project__title project__text">{project.title}</h4>
+            <h4 className="project__role project__text">{project.role}</h4>
+            <h4 className="project__company project__text">{project.productionCompany}</h4>
+            <h4 className="project__venue project__text">{project.venue}</h4>
         </div>
     );
 }
--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -15,41 +15,70 @@ export default function Projects() {
     let allProjects = [];
 
     // TOGGLE LOGIC FOR PROJECTS ACCORDION
-    const [isShowTVProjects, setIsShowTVProjects] = useState(null);
-    const [isShowFilmProjects, setIsShowFilmProjects] = useState(null);
-    const [isShowTheatreProjects, setIsShowTheatreProjects] = useState(null);
-    const [isShowOtherProjects, setIsShowOtherProjects] = useState(null);
+    const [isShowTVProjects, setIsShowTVProjects] = useState(false);
+    const [isShowFilmProjects, setIsShowFilmProjects] = useState(false);
+    const [isShowTheatreProjects, setIsShowTheatreProjects] = useState(false);
+    const [isShowOtherProjects, setIsShowOtherProjects] = useState(false);
 
     // ACTIVE HEADER
     const [isActiveHeader, setIsActiveHeader] = useState(null);
     // logic feels like it's on the tip of my tongue, but trying to use this^ state to hold the active header value, and when the header loses this active status, need it to toggle itself off!
 
-    // const collapseHeaders = () => {
-    // LOGIC FOR LOADING PROJECTS
-    if (isActiveHeader === isShowTVProjects) {
-        allProjects = projects.filter((project) => project.category === "tv");
-        console.log("Collapse TV:", allProjects);
-    } else if (isActiveHeader === isShowFilmProjects) {
-        allProjects = projects.filter((project) => project.category === "film");
-        console.log("Collapse FILM:", allProjects);
-    } else if (isActiveHeader === isShowTheatreProjects) {
-        allProjects = projects.filter((project) => project.category === "theatre");
-        console.log("Collapse THEATRE:", allProjects);
-    } else if (isActiveHeader === isShowOtherProjects) {
-        allProjects = projects.filter((project) => project.category === "other");
-        console.log("Collapse OTHER:", allProjects);
-    } else {
-        console.log("No Projects have been selected from the accordion")
-    }
-    // }
+    // create function for logic, nest in function for setting active header?
+    const makeActiveHeader = () => {
+        if (isShowTVProjects) {
+            setIsActiveHeader(isShowFilmProjects);
+            showAccordionContent();
+        } else if (isShowFilmProjects) {
+            setIsActiveHeader(isShowFilmProjects);
+            showAccordionContent();
+        } else if (isShowTheatreProjects) {
+            setIsActiveHeader(isShowTheatreProjects)
+            showAccordionContent();
+        } else if (isShowOtherProjects) {
+            setIsActiveHeader(isShowOtherProjects)
+            showAccordionContent();
+        } else {
+            console.log("No Header Selected.");
 
+        }
+    }
+
+    const showAccordionContent = () => {
+        // LOGIC FOR LOADING PROJECTS
+        if (isActiveHeader === isShowTVProjects) {
+            allProjects = projects.filter((project) => project.category === "tv");
+            if (isActiveHeader !== isShowTVProjects) {
+                setIsShowTVProjects(null);
+            }
+        } else if (isActiveHeader === isShowFilmProjects) {
+            allProjects = projects.filter((project) => project.category === "film");
+            if (isActiveHeader !== isShowFilmProjects) {
+                setIsShowFilmProjects(null);
+            }
+        } else if (isActiveHeader === isShowTheatreProjects) {
+            allProjects = projects.filter((project) => project.category === "theatre");
+            if (isActiveHeader !== isShowTheatreProjects) {
+                setIsShowTheatreProjects(null);
+            }
+        } else if (isActiveHeader === isShowOtherProjects) {
+            allProjects = projects.filter((project) => project.category === "other");
+            if (isActiveHeader !== isShowOtherProjects) {
+                setIsShowOtherProjects(null);
+            }
+        } else {
+            console.log("No Projects have been selected from the accordion")
+        }
+    }
     return (
         <section className="projects">
             <h2 className="projects__title">Projects</h2>
             <div className="projects__accordion">
                 <div className="projects__tv">
                     <button onClick={() => {
-                        setIsShowTVProjects(!isShowTVProjects); collapseHeaders;
+                        setIsShowTVProjects(!isShowTVProjects); makeActiveHeader();
+                        console.log(isShowTVProjects);
+
                     }}
                         className="tv__title">Television</button>
                     {isShowTVProjects && (
@@ -65,11 +94,11 @@ export default function Projects() {
                 </div>
                 <div className="projects__film">
                     <button onClick={() => {
-                        setIsShowFilmProjects(!isShowFilmProjects); collapseHeaders();
+                        setIsShowFilmProjects(!isShowFilmProjects); makeActiveHeader();
                     }} className="film__title">Film</button>
                     {isShowFilmProjects && (
-                        <div className="tv__credits">
-                            <ul className="tv__credits-list">
+                        <div className="film__credits">
+                            <ul className="film__credits-list">
                                 {allProjects.map((project) => {
                                     return <Project key={project.id} project={project} />;
                                 })}
@@ -79,11 +108,11 @@ export default function Projects() {
                 </div>
                 <div className="projects__theatre">
                     <button onClick={() => {
-                        setIsShowTheatreProjects(!isShowTheatreProjects); collapseHeaders();
+                        setIsShowTheatreProjects(!isShowTheatreProjects); makeActiveHeader();
                     }} className="theatre__title">Theatre</button>
                     {isShowTheatreProjects && (
-                        <div className="tv__credits">
-                            <ul className="tv__credits-list">
+                        <div className="theatre__credits">
+                            <ul className="theatre__credits-list">
                                 {allProjects.map((project) => {
                                     return <Project key={project.id} project={project} />;
                                 })}
@@ -93,11 +122,11 @@ export default function Projects() {
                 </div>
                 <div className="projects__multimedia">
                     <button onClick={() => {
-                        setIsShowOtherProjects(!isShowOtherProjects); collapseHeaders();
+                        setIsShowOtherProjects(!isShowOtherProjects); makeActiveHeader();
                     }} className="multi__title">Other Media</button>
                     {isShowOtherProjects && (
-                        <div className="tv__credits">
-                            <ul className="tv__credits-list">
+                        <div className="other__credits">
+                            <ul className="other__credits-list">
                                 {allProjects.map((project) => {
                                     return <Project key={project.id} project={project} />;
                                 })}
