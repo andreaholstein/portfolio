d8c7888a5009403ae8057d96d4fb6aed8855e443
andrea <andrea.gitcommit@gmail.com>
2025-08-02 15:33:52 -0400

    Success - Loaded accordion data, correctly segmented under headers, in Projects

--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -14,6 +14,12 @@ export default function Projects() {
     // LOAD DATA
     let allProjects = [];
 
+    if (!projects) {
+        return <div className="projects__loading">Loading Data...</div>
+    } else {
+        allProjects = projects;
+    }
+
     // TOGGLE LOGIC FOR PROJECTS ACCORDION
     const [isShowTVProjects, setIsShowTVProjects] = useState(false);
     const [isShowFilmProjects, setIsShowFilmProjects] = useState(false);
@@ -21,70 +27,48 @@ export default function Projects() {
     const [isShowOtherProjects, setIsShowOtherProjects] = useState(false);
 
     // ACTIVE HEADER
-    const [isActiveHeader, setIsActiveHeader] = useState(null);
+    // const [isActiveHeader, setIsActiveHeader] = useState(null);
     // logic feels like it's on the tip of my tongue, but trying to use this^ state to hold the active header value, and when the header loses this active status, need it to toggle itself off!
 
     // create function for logic, nest in function for setting active header?
-    const makeActiveHeader = () => {
-        if (isShowTVProjects) {
-            setIsActiveHeader(isShowFilmProjects);
-            showAccordionContent();
-        } else if (isShowFilmProjects) {
-            setIsActiveHeader(isShowFilmProjects);
-            showAccordionContent();
-        } else if (isShowTheatreProjects) {
-            setIsActiveHeader(isShowTheatreProjects)
-            showAccordionContent();
-        } else if (isShowOtherProjects) {
-            setIsActiveHeader(isShowOtherProjects)
-            showAccordionContent();
-        } else {
-            console.log("No Header Selected.");
 
-        }
-    }
+    // LOGIC FOR LOADING PROJECTS
+    // const showHeaderContent = () => {
+    //     if (isShowTVProjects) {
+    //         allProjects = projects.filter((project) => project.category === "tv");
+    //     } if (isShowFilmProjects) {
+    //         allProjects = projects.filter((project) => project.category === "film");
+    //     } if (isShowTheatreProjects) {
+    //         allProjects = projects.filter((project) => project.category === "theatre");
 
-    const showAccordionContent = () => {
-        // LOGIC FOR LOADING PROJECTS
-        if (isActiveHeader === isShowTVProjects) {
-            allProjects = projects.filter((project) => project.category === "tv");
-            if (isActiveHeader !== isShowTVProjects) {
-                setIsShowTVProjects(null);
-            }
-        } else if (isActiveHeader === isShowFilmProjects) {
-            allProjects = projects.filter((project) => project.category === "film");
-            if (isActiveHeader !== isShowFilmProjects) {
-                setIsShowFilmProjects(null);
-            }
-        } else if (isActiveHeader === isShowTheatreProjects) {
-            allProjects = projects.filter((project) => project.category === "theatre");
-            if (isActiveHeader !== isShowTheatreProjects) {
-                setIsShowTheatreProjects(null);
-            }
-        } else if (isActiveHeader === isShowOtherProjects) {
-            allProjects = projects.filter((project) => project.category === "other");
-            if (isActiveHeader !== isShowOtherProjects) {
-                setIsShowOtherProjects(null);
-            }
-        } else {
-            console.log("No Projects have been selected from the accordion")
-        }
-    }
+    //     } if (isShowOtherProjects) {
+    //         allProjects = projects.filter((project) => project.category === "other");
+    //     }
+    //     else {
+    //         console.log("No Headers Selected.");
+
+    //         // setIsShowTVProjects(null);
+    //         // setIsShowFilmProjects(null);
+    //         // setIsShowTheatreProjects(null);
+    //         // setIsShowOtherProjects(null);
+    //     }
+    // }
+
+    // console.log("ShowTv", isShowTVProjects);
+
     return (
         <section className="projects">
             <h2 className="projects__title">Projects</h2>
             <div className="projects__accordion">
                 <div className="projects__tv">
                     <button onClick={() => {
-                        setIsShowTVProjects(!isShowTVProjects); makeActiveHeader();
-                        console.log(isShowTVProjects);
-
+                        setIsShowTVProjects(!isShowTVProjects);
                     }}
                         className="tv__title">Television</button>
                     {isShowTVProjects && (
                         <div className="tv__credits">
                             <ul className="tv__credits-list">
-                                {allProjects.map((project) => {
+                                {allProjects.filter((project) => project.category === "tv").map((project) => {
                                     return <Project key={project.id} project={project} />;
                                 })}
                             </ul>
@@ -94,12 +78,12 @@ export default function Projects() {
                 </div>
                 <div className="projects__film">
                     <button onClick={() => {
-                        setIsShowFilmProjects(!isShowFilmProjects); makeActiveHeader();
+                        setIsShowFilmProjects(!isShowFilmProjects);
                     }} className="film__title">Film</button>
                     {isShowFilmProjects && (
                         <div className="film__credits">
                             <ul className="film__credits-list">
-                                {allProjects.map((project) => {
+                                {allProjects.filter((project) => project.category === "film").map((project) => {
                                     return <Project key={project.id} project={project} />;
                                 })}
                             </ul>
@@ -108,12 +92,12 @@ export default function Projects() {
                 </div>
                 <div className="projects__theatre">
                     <button onClick={() => {
-                        setIsShowTheatreProjects(!isShowTheatreProjects); makeActiveHeader();
+                        setIsShowTheatreProjects(!isShowTheatreProjects);
                     }} className="theatre__title">Theatre</button>
                     {isShowTheatreProjects && (
                         <div className="theatre__credits">
                             <ul className="theatre__credits-list">
-                                {allProjects.map((project) => {
+                                {allProjects.filter((project) => project.category === "theatre").map((project) => {
                                     return <Project key={project.id} project={project} />;
                                 })}
                             </ul>
@@ -122,12 +106,12 @@ export default function Projects() {
                 </div>
                 <div className="projects__multimedia">
                     <button onClick={() => {
-                        setIsShowOtherProjects(!isShowOtherProjects); makeActiveHeader();
+                        setIsShowOtherProjects(!isShowOtherProjects);
                     }} className="multi__title">Other Media</button>
                     {isShowOtherProjects && (
                         <div className="other__credits">
                             <ul className="other__credits-list">
-                                {allProjects.map((project) => {
+                                {allProjects.filter((project) => project.category === "other").map((project) => {
                                     return <Project key={project.id} project={project} />;
                                 })}
                             </ul>
@@ -137,4 +121,4 @@ export default function Projects() {
             </div>
         </section>
     );
-}
+};
