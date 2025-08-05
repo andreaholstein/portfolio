2f38c9f3e9c90b9e6e87cc813e63942e6f780982
andrea <andrea.gitcommit@gmail.com>
2025-08-05 16:27:45 -0400

    Initialize Footer

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+// ---------- FXNALITY ----------
+// import { useState, useEffect } from 'react'
+// ---------- COMPONENTS ----------
+// ---------- STYLES ----------
+import "./Footer.scss";
+
+export default function Footer() {
+
+    return (
+        <footer className="footer">
+            <h2 className="footer__title title">footer</h2>
+
+        </footer>
+    );
+}
